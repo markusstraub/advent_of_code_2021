3108cea994754a18145dd86a34da403234b9e864
Markus Straub <markus.straub@ait.ac.at>
2021-12-03 20:03:11 +0100

    day 3 part 1

--- 03/advent03.py
+++ 03/advent03.py
@@ -1,2 +1,102 @@
-def juhu():
-    return 5
+#%%
+import itertools
+import operator
+import more_itertools
+
+test_input = """00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010"""
+
+#%%
+
+
+class DiagnosticBitSum:
+    @staticmethod
+    def zero():
+        return DiagnosticBitSum([0])
+
+    @staticmethod
+    def from_string(string):
+        int_array = [int(bit) for bit in list(string)]
+        return DiagnosticBitSum(int_array)
+
+    def __init__(self, int_array):
+        self.int_array = int_array
+
+    def __add__(self, other):
+        return DiagnosticBitSum(
+            [
+                a + b
+                for a, b in itertools.zip_longest(
+                    self.int_array, other.int_array, fillvalue=0
+                )
+            ]
+        )
+
+    def calc_gamma(self, record_count):
+        bits = [1 if val > (record_count - val) else 0 for val in self.int_array]
+        print(bits)
+        return DiagnosticBitSum(bits)
+
+    def calc_epsilon(self, record_count):
+        bits = [1 if val < (record_count - val) else 0 for val in self.int_array]
+        return DiagnosticBitSum(bits)
+
+    def calc_power_consumption(self, record_count):
+        return (
+            self.calc_gamma(record_count).to_decimal()
+            * self.calc_epsilon(record_count).to_decimal()
+        )
+
+    def to_decimal(self):
+        result = 0
+        current_bit_value = 1
+        for bit in reversed(self.int_array):
+            result += current_bit_value * bit
+            current_bit_value *= 2
+        return result
+
+    def __repr__(self):
+        strings = [str(bit) for bit in self.int_array]
+        return "".join(strings)
+
+
+def gen_diagnostics(filename=None):
+    """return list of bits (as int), test data if no file is given"""
+    if filename is None:
+        yield from (
+            DiagnosticBitSum.from_string(line) for line in test_input.split("\n")
+        )
+    else:
+        with open(filename) as file:
+            for line in file:
+                print(line)
+                yield DiagnosticBitSum.from_string(line.strip())
+
+
+filename = "advent03.txt"
+record_count = more_itertools.ilen(gen_diagnostics(filename))
+print(f"found {record_count} diagnostic bits.")
+
+accumulated = DiagnosticBitSum.zero()
+for bits in gen_diagnostics(filename):
+    print(repr(bits))
+    accumulated += bits
+
+print(f"accumulated: {repr(accumulated)}")
+
+power_consumption = accumulated.calc_power_consumption(record_count)
+print(f"part1: submarine power consumption is {power_consumption}.")
+
+# epsilon_rate = calc_epsilon_rate(accumulated, record_count)
+
+# %%
--- 03/test_advent03.py
+++ 03/test_advent03.py
@@ -1,5 +1,36 @@
-from advent03 import juhu
+from advent03 import DiagnosticBitSum, gen_diagnostics
+import os
 
 
-def test_juhu():
-    assert juhu() == 5
+def test_diagnostic_bits():
+    zero = DiagnosticBitSum.zero()
+    assert repr(zero) == "0"
+    bits_a = DiagnosticBitSum.from_string("01001")
+    bits_b = DiagnosticBitSum([1, 1, 1, 0, 0])
+
+    assert repr(zero + bits_a) == "01001"
+    assert repr(bits_a + zero) == "01001"
+    assert repr(bits_a + bits_b) == "12101"
+
+
+def test_to_decimal():
+    assert DiagnosticBitSum.from_string("00000").to_decimal() == 0
+    assert DiagnosticBitSum.from_string("00001").to_decimal() == 1
+    assert DiagnosticBitSum.from_string("01010").to_decimal() == 10
+    assert DiagnosticBitSum.from_string("11111").to_decimal() == 31
+
+
+def test_rates_and_power_consumption():
+    accumulated = DiagnosticBitSum.zero()
+    for bits in gen_diagnostics():
+        accumulated += bits
+
+    gamma = accumulated.calc_gamma(record_count=12)
+    assert repr(gamma) == "10110"
+    assert gamma.to_decimal() == 22
+
+    epsilon = accumulated.calc_epsilon(record_count=12)
+    assert repr(epsilon) == "01001"
+    assert epsilon.to_decimal() == 9
+
+    assert accumulated.calc_power_consumption(record_count=12) == 198
