97c574da258a0c3b798aff512e0038aa199cb01c
Markus Straub <markus.straub@ait.ac.at>
2021-12-08 23:21:46 +0100

    day 8 part 2 in ~80 mins

--- 08/advent08.py
+++ 08/advent08.py
@@ -1,6 +1,3 @@
-#%%
-import sys
-import itertools
 import more_itertools
 from collections import namedtuple
 
@@ -15,11 +12,8 @@ def gen_note_entries(filename="test_advent08.txt"):
             yield NoteEntry(tokens[0].split(" "), tokens[1].split(" "))
 
 
-# %%
-print(list(gen_note_entries()))
-
-
 def count_1478(entries):
+    """simply count how often 1, 4, 7 or 8 are contained in the output"""
     count = 0
     for entry in entries:
         by_len = more_itertools.map_reduce(entry.output, keyfunc=len, reducefunc=len)
@@ -28,7 +22,70 @@ def count_1478(entries):
     return count
 
 
-count = count_1478(gen_note_entries("advent08.txt"))
-print(f"part1: found {count} instances of 1, 4, 7 or 8 in the entries' outputs")
+def subtract_string(stringa, stringb):
+    return "".join(set(stringa).difference(set(stringb)))
 
-# %%
+
+def is_subset(stringa, stringb):
+    return set(stringa).issubset(set(stringb))
+
+
+def decode(entry):
+    by_len = more_itertools.map_reduce(entry.patterns, keyfunc=len)
+    digit2code = {}
+
+    # decode trivial parts
+    digit2code[1] = by_len.get(2)[0]
+    digit2code[4] = by_len.get(4)[0]
+    digit2code[7] = by_len.get(3)[0]
+    digit2code[8] = by_len.get(7)[0]
+
+    # 0, 6, 9 use six segments each
+    candidates = by_len.get(6)
+
+    code = filter(lambda x: is_subset(digit2code[4], x), candidates)
+    digit2code[9] = list(code)[0]
+
+    subtracted = subtract_string(digit2code[8], digit2code[7])
+    code = filter(lambda x: is_subset(subtracted, x), candidates)
+    digit2code[6] = list(code)[0]
+
+    remaining = set(candidates).difference([digit2code[6], digit2code[9]])
+    digit2code[0] = list(remaining)[0]
+
+    # 2, 3, 5 use five segments each
+    candidates = by_len.get(5)
+
+    code = filter(lambda x: is_subset(digit2code[1], x), candidates)
+    digit2code[3] = list(code)[0]
+
+    subtracted = subtract_string(digit2code[9], digit2code[1])
+    code = filter(lambda x: is_subset(subtracted, x), candidates)
+    digit2code[5] = list(code)[0]
+
+    remaining = set(candidates).difference([digit2code[3], digit2code[5]])
+    digit2code[2] = list(remaining)[0]
+
+    return _convert(digit2code, entry.output)
+
+
+def _sort(code):
+    return "".join(sorted(set(code)))
+
+
+def _intlist2int(list):
+    return int("".join([str(i) for i in list]))
+
+
+def _convert(digit2code, codes):
+    code2digit = {_sort(v): k for k, v in digit2code.items()}
+    ints = [code2digit[_sort(code)] for code in codes]
+    return _intlist2int(ints)
+
+
+if __name__ == "__main__":
+    count = count_1478(gen_note_entries("advent08.txt"))
+    print(f"part1: found {count} instances of 1, 4, 7 or 8 in the entries' outputs")
+
+    decoded = [decode(entry) for entry in gen_note_entries("advent08.txt")]
+    print(f"part2: sum of all decoded outputs is {sum(decoded)}.")
--- 08/test_advent08.py
+++ 08/test_advent08.py
@@ -1,12 +1,18 @@
-from advent08 import gen_note_entries, count_1478
+from advent08 import gen_note_entries, count_1478, decode
 
 
 def test_generator():
     entries = list(gen_note_entries())
-    assert len(entries) == 10
+    assert len(entries) == 11
     assert len(entries[0].patterns) == 10
     assert len(entries[0].output) == 4
 
 
 def test_count_1478():
     assert count_1478(gen_note_entries()) == 26
+
+
+def test_decode():
+    entries = list(gen_note_entries())
+    decoded = [decode(entry) for entry in entries]
+    assert decoded == [5353, 8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
