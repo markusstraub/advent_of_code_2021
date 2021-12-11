31401c6e615f65eaf38d5de8f2f57c3668a7007b
Markus Straub <markus.straub@ait.ac.at>
2021-12-11 15:19:42 +0100

    day 10, part 2, ~30mins

--- 10/advent10.py
+++ 10/advent10.py
@@ -1,38 +1,73 @@
-#%%
 from collections import namedtuple
 
 
-SCORES = {")": 3, "]": 57, "}": 1197, ">": 25137}
-OPENING = "({[<"
+CORRUPTION_SCORES = {")": 3, "]": 57, "}": 1197, ">": 25137}
+AUTOCOMPLETION_SCORES = {")": 1, "]": 2, "}": 3, ">": 4}
+OPENING_BRACKETS = "({[<"
 OPEN2CLOSE = {"(": ")", "[": "]", "{": "}", "<": ">"}
+TEST_FILENAME = "test_advent10.txt"
 
 
-def read_input(filename="test_advent10.txt"):
+def read_input(filename=TEST_FILENAME):
     """return test data if no file is given"""
     with open(filename) as file:
         for line in file:
             yield line.strip()
 
 
-def check_validity(nav_chunk):
+def calc_corruption_score(nav_line):
     stack = list()
-    for bracket in nav_chunk:
-        if bracket in OPENING:
+    for bracket in nav_line:
+        if bracket in OPENING_BRACKETS:
             stack.append(bracket)
         else:
             if len(stack) == 0:
-                return SCORES.get(bracket)
+                return CORRUPTION_SCORES.get(bracket)
             open_bracket = stack.pop()
             if not OPEN2CLOSE[open_bracket] == bracket:
-                return SCORES.get(bracket)
+                return CORRUPTION_SCORES.get(bracket)
     return 0
 
 
-#%%
+def calc_autocompletion_score(nav_line):
+    """returns 0 for corrupt lines"""
+    if calc_corruption_score(nav_line) > 0:
+        return 0
+    stack = list()
+    for bracket in nav_line:
+        if bracket in OPENING_BRACKETS:
+            stack.append(bracket)
+        else:
+            if len(stack) == 0:
+                raise Exception(f"invalid nav line {nav_line}")
+            open_bracket = stack.pop()
+            if not OPEN2CLOSE[open_bracket] == bracket:
+                raise Exception(f"invalid nav line {nav_line}")
+
+    # autocomplete open brackets
+    score = 0
+    autocomplete = ""
+    for open_bracket in reversed(stack):
+        closing_bracket = OPEN2CLOSE.get(open_bracket)
+        autocomplete += closing_bracket
+        score *= 5
+        score += AUTOCOMPLETION_SCORES.get(closing_bracket)
+    # print(f"autocompleted: {nav_line} + {autocomplete}")
+    return score
+
+
+def calc_middle_autocompletion_score(filename=TEST_FILENAME):
+    scores = [calc_autocompletion_score(line) for line in read_input(filename)]
+    scores = list(sorted(filter(None, scores)))
+    return scores[int(len(scores) / 2)]
+
 
 if __name__ == "__main__":
-    total_error = sum([check_validity(chunk) for chunk in read_input("advent10.txt")])
-    print(f"part1: total score for corrupted lines: {total_error}")
+    input_file = "advent10.txt"
+    total_error = sum(
+        [calc_corruption_score(nav_line) for nav_line in read_input(input_file)]
+    )
+    print(f"part1: total score for corrupted navigation chunks: {total_error}")
 
-
-# %%
+    middle_autocompletion_score = calc_middle_autocompletion_score(input_file)
+    print(f"part2: middle autocompletion score is {middle_autocompletion_score}")
--- 10/test_advent10.py
+++ 10/test_advent10.py
@@ -1,16 +1,30 @@
-from advent10 import read_input, check_validity
+from advent10 import (
+    read_input,
+    calc_corruption_score,
+    calc_autocompletion_score,
+    calc_middle_autocompletion_score,
+)
 
 
-def test_validity():
-    assert check_validity(")") == 3
-    assert check_validity("())") == 3
-    assert check_validity("]") == 57
-    assert check_validity("()]") == 57
-    assert check_validity("}") == 1197
-    assert check_validity("()}") == 1197
-    assert check_validity(">") == 25137
-    assert check_validity("()>") == 25137
+def test_calc_corruption_score():
+    assert calc_corruption_score(")") == 3
+    assert calc_corruption_score("())") == 3
+    assert calc_corruption_score("]") == 57
+    assert calc_corruption_score("()]") == 57
+    assert calc_corruption_score("}") == 1197
+    assert calc_corruption_score("()}") == 1197
+    assert calc_corruption_score(">") == 25137
+    assert calc_corruption_score("()>") == 25137
 
 
-def test_validity_for_test_input():
-    assert sum([check_validity(chunk) for chunk in read_input()]) == 26397
+def test_calc_corruption_score_for_test_input():
+    assert sum([calc_corruption_score(nav_line) for nav_line in read_input()]) == 26397
+
+
+def test_calc_autocompletion_score():
+    assert calc_autocompletion_score("[({(<(())[]>[[{[]{<()<>>") == 288957
+    # complete by adding }}]])})]
+
+
+def test_calc_middle_autocompletion_score():
+    assert calc_middle_autocompletion_score() == 288957
